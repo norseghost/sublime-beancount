4ee59df9a2ffb2a4b5b46312b2fc5e8740d0d8d7
Vincent Vu <vqv@me.com>
2021-05-10 02:03:47 -0400

    Preliminary decimal alignment

--- completions.py
+++ completions.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 import subprocess
 import pickle
+import re
 
 def sans_root(account_name):
     components = account_name.split(':')[1:]
@@ -19,6 +20,7 @@ class BeancountCompletions(sublime_plugin.EventListener):
     def __init__(self):
         self.beanfile = None
         self.index = None
+        super().__init__()
 
     def update_index(self, view):
         # Update index if new beanfile
@@ -75,15 +77,58 @@ class BeancountCompletions(sublime_plugin.EventListener):
 
         return out
 
-    def guess_decimal_column(self, view):
-        regions = view.find_by_selector("meta.amount.beancount")
-        for r in regions:
-            print(view.substr(r))
+def get_decimal_offset(str):
+    m = re.search(r'[0-9](?=[.\s])', str)
+    if not m:
+        return None
+    return m.start() + 1
 
-    def on_query_context(self, view, key, operator, operand, match_all):
-        if view.syntax().scope != "source.beancount":
+def guess_decimal_column(view):
+    regions = view.find_by_selector("meta.amount.posting.beancount")
+    decimal_col = 0
+    n = 0
+    for r in regions:
+        amount = view.substr(r)
+        offset = get_decimal_offset(amount)
+        if not offset:
+            continue        
+        decimal_col = (decimal_col * n + view.rowcol(r.a + offset)[1]) / (n + 1)
+        n = n + 1
+    return int(decimal_col)
+
+class BeancountAlignDecimalCommand(sublime_plugin.TextCommand):
+    def __init__(self, view):
+        self.decimal_column = None
+        super().__init__(view)
+
+    def run(self, edit):
+        if not len(self.view.sel()) == 1 and self.view.sel()[0].a == self.view.sel()[0].b:
             return
 
-        # self.guess_decimal_column(view)
-        return False
+        loc = self.view.sel()[0].a
+        if not self.view.match_selector(loc, "source.beancount & meta.amount.posting.beancount"):
+            return
 
+        region = self.view.extract_scope(loc)
+
+        # Locate the decimal position of the region
+        amount = self.view.substr(region)
+        region_decimal_col = self.view.rowcol(region.a + get_decimal_offset(amount))[1]
+
+        # Get setting
+        if not self.decimal_column:
+            self.decimal_column = guess_decimal_column(self.view)
+
+        offset = self.decimal_column - region_decimal_col
+
+        # Insert spaces
+        if offset >= 0:
+            self.view.insert(edit, region.a, ' ' * offset)
+            return
+        else:
+            region.b = region.a
+            region.a += offset - 1
+            candidate = self.view.substr(region)
+            if candidate.isspace():
+                region.a += 1
+                self.view.erase(edit, region)
