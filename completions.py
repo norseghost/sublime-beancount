819bc8ebb15ca4395b16d8aec643302b7e65c444
Vincent Vu <vqv@me.com>
2021-10-19 09:48:19 -0400

    Update

--- completions.py
+++ completions.py
@@ -3,6 +3,7 @@ import sublime_plugin
 import subprocess
 import pickle
 import re
+import time
 
 def sans_root(account_name):
     components = account_name.split(':')[1:]
@@ -20,22 +21,41 @@ class BeancountCompletions(sublime_plugin.EventListener):
     def __init__(self):
         self.beanfile = None
         self.index = None
+        self.options_map_dump = None
         super().__init__()
 
     def update_index(self, view):
         # Update index if new beanfile
-        view_beanfile = view.settings().get('beanfile')
-        if view_beanfile != self.beanfile:
-            self.beanfile = view_beanfile
-            self.update_index()
+        self.beanfile = view.settings().get('beanfile')
 
-        cmd = ["/usr/bin/env", "python3", "sublime-beancount/util/index_completions.py", self.beanfile]
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        if not self.beanfile:
+            return
 
-        self.index = pickle.load(proc.stdout)
-        self.index['meta.account.beancount'].update(sub_accounts(self.index['meta.account.beancount']))
+        if view.settings().get('beancount_debug'):
+            print("sublime-beancount: Updating index")
 
-        print("sublime-beancount: Updated index")
+        input = {
+            'beanfile': self.beanfile,
+            'options_map_dump': self.options_map_dump
+        }
+        index_completions =sublime.packages_path() + "/sublime-beancount/util/index_completions.py"
+        cmd = ["/usr/bin/env", "python3", index_completions, self.beanfile]        # cmd = ["/usr/bin/env", "pwd"]
+        # print(cmd)
+        proc = subprocess.run(cmd, input=pickle.dumps(input), capture_output = True)
+        # print(proc.returncode)
+        # print(proc.stderr)
+        # print(proc.stdout)
+        # return
+        output = pickle.loads(proc.stdout)
+
+        if output:
+            self.index = output['index']
+            self.options_map_dump = output['options_map_dump']
+            self.index['meta.account.beancount'].update(sub_accounts(self.index['meta.account.beancount']))
+
+        if view.settings().get('beancount_debug'):
+            print("sublime-beancount: Updated index")
+
         return
 
     def on_load_async(self, view):
@@ -66,8 +86,8 @@ class BeancountCompletions(sublime_plugin.EventListener):
 
         for k in self.index.keys():
             if view.match_selector(locations[0], k):
-                # print("sublime-beancount: Completion for " + k)
-                # print(self.index[k])
+                if view.settings().get('beancount_debug'):
+                    print("sublime-beancount: Getting completions for " + prefix + " in " + k)
                 if len(prefix):
                     for comp in self.index[k]:
                         if comp.lower().startswith(prefix):
@@ -84,7 +104,7 @@ def get_decimal_offset(str):
     return m.start() + 1
 
 def guess_decimal_column(view):
-    regions = view.find_by_selector("meta.amount.posting.beancount")
+    regions = view.find_by_selector("source.beancount & (meta.amount.beancount - meta.metadata.value.beancount)")
     decimal_col = 0
     n = 0
     for r in regions:
@@ -102,33 +122,43 @@ class BeancountAlignDecimalCommand(sublime_plugin.TextCommand):
         super().__init__(view)
 
     def run(self, edit):
-        if not len(self.view.sel()) == 1 and self.view.sel()[0].a == self.view.sel()[0].b:
-            return
+        for region in self.view.sel():
+            if not region.empty():
+                continue
+            loc = region.begin()
+            if not self.view.match_selector(loc, "source.beancount & (meta.amount.beancount - meta.metadata.value.beancount)"):
+                continue
 
-        loc = self.view.sel()[0].a
-        if not self.view.match_selector(loc, "source.beancount & meta.amount.posting.beancount"):
-            return
+            scope_region = self.view.extract_scope(loc)
 
-        region = self.view.extract_scope(loc)
+            # Locate the decimal position of the region
+            amount = self.view.substr(scope_region)
+            region_decimal_col = self.view.rowcol(scope_region.a + get_decimal_offset(amount))[1]
 
-        # Locate the decimal position of the region
-        amount = self.view.substr(region)
-        region_decimal_col = self.view.rowcol(region.a + get_decimal_offset(amount))[1]
+            # Get setting
+            self.decimal_column = self.view.settings().get('beancount_decimal_column')
+            if not self.decimal_column:
+                self.decimal_column = guess_decimal_column(self.view)
 
-        # Get setting
-        if not self.decimal_column:
-            self.decimal_column = guess_decimal_column(self.view)
+            offset = self.decimal_column - region_decimal_col
 
-        offset = self.decimal_column - region_decimal_col
+            # Insert spaces
+            if offset >= 0:
+                self.view.insert(edit, scope_region.a, ' ' * offset)
+                return
+            else:
+                scope_region.b = scope_region.a
+                scope_region.a += offset - 1
+                candidate = self.view.substr(scope_region)
+                if candidate.isspace():
+                    scope_region.a += 1
+                    self.view.erase(edit, scope_region)
 
-        # Insert spaces
-        if offset >= 0:
-            self.view.insert(edit, region.a, ' ' * offset)
-            return
-        else:
-            region.b = region.a
-            region.a += offset - 1
-            candidate = self.view.substr(region)
-            if candidate.isspace():
-                region.a += 1
-                self.view.erase(edit, region)
+class BeancountInsertDateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for s in view.sel():
+            if s.empty():
+                view.insert(edit, s.a, time.strftime('%Y-%m-%d'))
+            else:
+                view.replace(edit, s, time.strftime('%Y-%m-%d'))
+
